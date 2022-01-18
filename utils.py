5944c5161a30b63bddc2eecd8134394f8a631250
tangze <mf1832146@smail.nju.edu.cn>
2022-01-18 17:27:44 +0800

    [update]

--- utils.py
+++ utils.py
@@ -81,10 +81,10 @@ def load_and_cache_gen_data_from_db(args, pool, tokenizer, split_tag):
         tuple_examples = [(example, idx, tokenizer, args, split_tag, db_name) for idx, example in enumerate(examples)]
         features = []
         for tuple_example in tqdm(tuple_examples, total=len(tuple_examples)):
-            features.append(pool.apply_async(convert_example_to_func_naming_feature, args(tuple_example,)))
+            features.append(pool.apply_async(convert_example_to_func_naming_feature, args=(tuple_example,)))
         #    features.append(convert_example_to_func_naming_feature(tuple_example))
-        features = pool.map(convert_example_to_func_naming_feature,
-                            tqdm(tuple_examples, total=len(tuple_examples)))
+        # features = pool.map(convert_example_to_func_naming_feature,
+        #                     tqdm(tuple_examples, total=len(tuple_examples)))
         data = FuncNamingDataset(features, db_name, args, tokenizer)
         if args.local_rank in [-1, 0]:
             torch.save(data, cache_fn)
